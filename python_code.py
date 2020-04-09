cf3e6808d5f27d314dc85176f49013d21f048696
unknown <sandeepkumar.v0001@gmail.com>
2020-04-09 19:03:10 +0530

    adds python script to github

---
+++ python_code.py
@@ -0,0 +1,7 @@
+
+
+print("Hello Github!")
+Sandeep Kumar@DESKTOP-074TGLH MINGW64 /c/users/Sandeep Kumar/.ssh
+$ cd ~/Desktop/Projects
+Sandeep Kumar@DESKTOP-074TGLH MINGW64 /c/users/Sandeep Kumar/.ssh
+$ cd ~/Desktop/Projects
